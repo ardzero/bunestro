f5958f0099f1d7ab97388ba6dbc8c9d4b3064c17
ardzero <farhanchowdhury6543@gmail.com>
2026-05-17 03:31:15 +0600

    fonts system changed to new astro v6 and use intersection hook added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
@@ -14,4 +14,42 @@ export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	fonts: [
+		{
+			provider: fontProviders.google(),
+			name: "Inter",
+			cssVariable: "--font-inter",
+			weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+			fallbacks: ["sans-serif"],
+		},
+
+		{
+			provider: fontProviders.local(),
+			name: "Satoshi",
+			cssVariable: "--font-satoshi",
+			fallbacks: ["sans-serif"],
+			options: {
+				variants: [
+					{
+						weight: "300 900",
+						style: "normal",
+						display: "swap",
+						src: [
+							"./src/assets/fonts/Satoshi-Variable.woff2",
+							"./src/assets/fonts/Satoshi-Variable.woff",
+						],
+					},
+					{
+						weight: "300 900",
+						style: "italic",
+						display: "swap",
+						src: [
+							"./src/assets/fonts/Satoshi-VariableItalic.woff2",
+							"./src/assets/fonts/Satoshi-VariableItalic.woff",
+						],
+					},
+				],
+			},
+		},
+	],
 });
